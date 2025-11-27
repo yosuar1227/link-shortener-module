f401d5a189a8debac82ee8ca0ed9550d27ca0790
yosuar1227 <yosuar.mieles@unicolombo.edu.co>
2025-11-26 22:47:30 -0500

    fix: fixing error setting the rest api id

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,7 +72,7 @@ resource "aws_api_gateway_resource" "shortenLinkGtwResource" {
 }
 //METHOD
 resource "aws_api_gateway_method" "shortenLinkGtwMethod" {
-  rest_api_id = aws_api_gateway_resource.shortenLinkGtwResource.id
+  rest_api_id = aws_api_gateway_rest_api.shortenLinkGtw.id
   resource_id = aws_api_gateway_resource.shortenLinkGtwResource.id
   http_method = var.HTTP_METHOD_POST
   authorization = var.NONE_AUTH
